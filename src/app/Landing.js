d5858193c76715c7fd10ba867c1830776aad87db
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-23 12:50:42 -0600

    Create landing for comapny info w bootstrap

--- src/app/Landing.js
+++ src/app/Landing.js
@@ -19,8 +19,19 @@ export class Landing extends Component {
 
     render() {
         return (
-            <div className='text'>
-                Landing
+            <div className='container-flex'>
+                <div className='row'>
+                    <div className='col justify-content-center'>
+                        <h6>Am I ready to raise capital?</h6>
+                        <form>
+                            <input type='text' id='company' className='form-control textInput' placeholder='*Company' onChange={this.textChange} />
+                            <input type='text' id='name' className='form-control textInput' placeholder='*Name' onChange={this.textChange} />
+                            <input type='email' id='email' className='form-control textInput' placeholder='*Email' onChange={this.textChange} />
+                        </form>
+                        <p className='subtext'>We will email you your results!</p>
+                        <button className='btn button-border'>Next <strong> > </strong></button>
+                    </div>
+                </div>
             </div>
         )
     }
