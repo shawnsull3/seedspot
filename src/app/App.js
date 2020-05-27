e6e2b4a99aa7c78e9b1e54906e7aa6f2ef233988
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-27 10:42:57 -0600

    Implement React Router for landing & main comps

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 import '../styles/App.css';
 import ReadyToRaiseCapital from './widgets/ReadyToRaiseCapital';
+import MainPage from './widgets/MainPage';
 
 function App() {
   return (
-    <div className="container h-100">
-      <div className="d-flex justify-content-center">
-        <ReadyToRaiseCapital />
+    <div className='container h-100'>
+      <div className='d-flex justify-content-center'>
+        <Switch>
+          <Route exact path='/' component={ReadyToRaiseCapital} />
+          <Route exact path='/main' component={MainPage} />
+        </Switch>
       </div>
 	  </div>
   );
