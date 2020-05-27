a171fe370effc9d7d2f8df884bf0d4bb47c62677
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-27 17:45:18 -0600

    Add styling & formatting

--- src/app/widgets/MainPage.js
+++ src/app/widgets/MainPage.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import '../../styles/App.css';
 import CompanyProfile from './CompanyProfile';
 import Investors from './Investors';
 import Tools from './Tools';
 import FAQs from './FAQs';
 import NavigationMenu from '../NavigationMenu';
+import '../../styles/App.css';
+import '../../styles/MainPage.css';
 
 const MainPage = () => {
     return (
-      <div className='container h-100'>
-        <div className='row'>
-          <div className='col-2'>
+      <div className='container'>
+        <div className='row full-height'>
+          <div className='col-3'>
             <NavigationMenu />  
           </div>
-          <div className='col-10'>
+          <div className='col-9 main'>
             <Switch>
               <Route path='/main/profile' component={CompanyProfile} />
               <Route path='/main/investors' component={Investors} />
