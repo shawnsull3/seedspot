ce72f9d7796452b6e6a52a9f63d89844cf0e9c21
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-27 17:45:57 -0600

    Clean up code for readability

--- src/app/common/Form.js
+++ src/app/common/Form.js
@@ -7,12 +7,23 @@ const Form = ({ fields, handleInput }) => {
             {fields.map( field => {
                 return (
                     field.type === 'text' || field.type === 'email' || field.type === 'password'
-                      ? <input type={field.type} id={field.id} className='form-control inputField' placeholder={field.placeholder} onChange={handleInput} />
-                      : field.type === 'dropdown'
-                        && <select id={field.id} className='form-control inputField' onChange={handleInput} >
-                            <option value="" selected="selected" disabled>{field.placeholder}</option>
+                      ? <input 
+                            type={field.type} 
+                            id={field.id} 
+                            className='form-control inputField' 
+                            placeholder={field.placeholder} 
+                            onChange={handleInput} 
+                            key={field.id} />
+                      : field.options
+                        && <select 
+                              id={field.id} 
+                              className='form-control inputField' 
+                              onChange={handleInput} 
+                              key={field.placeholder} 
+                            >
+                            <option value="" selected="selected" disabled >{field.placeholder}</option>
                             {field.options.map( option => (
-                                <option value={option}>{option}</option>
+                                <option value={option} key={option}>{option}</option>
                             ))}
                         </select>
                 )
