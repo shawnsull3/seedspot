3bf0d48b9049d28d8d3565d14f833eddb8682232
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-11 13:16:45 -0600

    Export sendEmail func for execution w htmlStr

--- src/app/common/sendGrid.js
+++ src/app/common/sendGrid.js
@@ -2,13 +2,16 @@
 // https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'sullivansm3@gmail.com',
-  from: 'sullivansm3@gmail.com',
-  subject: 'check check 1 2 1 2',
-  text: 'check',
-  html: '<strong>check</strong>',
-};
-sgMail.send(msg)
-  .then( data => console.log(data))
-  .catch( error => console.log(error, error.response.body));
+
+export default function sendEmail(htmlStr) {
+  const msg = {
+    to: 'sullivansm3@gmail.com',
+    from: 'sullivansm3@gmail.com',
+    subject: 'Startup Report Card',
+    text: 'check',
+    html: '<strong>check</strong>',
+  };
+  sgMail.send(msg)
+    .then( data => console.log(data))
+    .catch( error => console.log(error, error.response.body));
+}
