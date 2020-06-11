16be52395451f59cf1c7dafbc5c4baff8de115cf
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-10 18:55:29 -0600

    Require all input fields. Allow number types

--- src/app/common/Form.js
+++ src/app/common/Form.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import '../../styles/Form.css'
+import React from 'react';
+import '../../styles/Form.css';
 
 const Form = ({ fields, handleInput, state }) => {
     return (
         <form>
             {fields.map( field => {
                 return (
-                    field.type === 'text' || field.type === 'email' || field.type === 'password'
+                    field.type === 'text' || field.type === 'email' || field.type === 'password' || field.type === 'number'
                       ? <input 
                             type={field.type} 
                             id={field.id}
-                            value={state[field.id]} 
+                            value={state[field.id]}
+                            min={field.min}
+                            max={field.max} 
                             className='form-control inputField' 
                             placeholder={field.placeholder} 
                             onChange={handleInput} 
-                            key={field.id} />
+                            key={field.id} 
+                            required />
                       : field.options
                         && <div className='inputField' key={field.placeholder}>
                           <div className='select-arrow'>
@@ -22,7 +25,8 @@ const Form = ({ fields, handleInput, state }) => {
                                 id={field.id} 
                                 value={state[field.id]}
                                 className='form-control selectBox' 
-                                onChange={handleInput}  
+                                onChange={handleInput}
+                                required  
                                 >
                                 <option value="" selected disabled >{field.placeholder}</option>
                                 {field.options.map( option => (
